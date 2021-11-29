214c22e7bf544f776de747da65f2cafa5107badb
beefybroccoli <tedkimemail@yahoo.com>
2021-11-28 16:55:39 -0800

    modified server.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const server = require('./api/server');
+const server = require("./api/server");
 
-const port = 9000;
+const PORT = 9000;
 
-// START YOUR SERVER HERE
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
